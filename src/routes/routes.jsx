0d38ed71897f67392024c5eb57d652b94113eb3d
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2025-07-15 22:20:37 +0600

    modied

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -8,6 +8,7 @@ import Root from "./Root";
 import MainContent from "../compoents/Main/MainContent";
 import AuthSignInCover from "../compoents/SignIn/AuthSingInCover";
 import HotelProfile from "../pages/HotelProfile/HotelProfile";
+import AddNewRoom from '../pages/RoomManagement/AddNewRoom/AddNewRoom';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
         <Route path="" element={<Root />}>
           <Route index element={<MainContent />} />
           <Route path="hotel-profile" element={<HotelProfile />} />
+          <Route path="add-room" element={<AddNewRoom></AddNewRoom>}></Route>
         </Route>
       </Route>
     </Route>
