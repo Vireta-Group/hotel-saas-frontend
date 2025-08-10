48b2d9cc22dc98c50118bb769522b3160483d32a
omur faruk <omurfaruk39bd@gmail.com>
2025-08-10 23:40:54 +0600

    edit variable and submitButton and index.css

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,43 +1,26 @@
 // Demo.jsx
 import { useRef } from "react";
 import Input from "./ui/input/Input";
-import SubmitButton from "./ui/submitButton/SubmitButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Demo() {
   const inputRef = useRef();
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(inputRef.current?.value);
-  };
-
   const style = {
-    height: "100vh",
+    height: "500px",
+    width: "500px",
     display: "flex",
+    backgroundColor: "#e6e7ee",
     justifyContent: "center",
     alignItems: "center",
   };
 
   return (
-    <div className="d-flex justify-content-center " style={style}>
-      <form onSubmit={submitHandler} className="card">
-        <Input
-          label="Enter your name"
-          id="name"
-          isFullScreen={true}
-          isRequire={true}
-          type="text"
-          ref={inputRef}
-        />
-        <Input
-          label="Enter your email"
-          id="email"
-          isFullScreen={true}
-          isRequire={true}
-          type="email"
-        />
-        <SubmitButton innerText="Submit" width="300px" />
-      </form>
+    <div className="d-flex justify-content-center" style={style}>
+      <Input iconClass="bi bi-alarm" ref={inputRef}>
+        <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
+      </Input>
     </div>
   );
 }
